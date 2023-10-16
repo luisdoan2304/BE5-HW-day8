e30583286624c3e6439768e478fedee39cc708c2
luisdoan2304 <93071923+luisdoan2304@users.noreply.github.com>
2023-10-16 17:51:00 +1100

    Add files via upload

---
+++ Mentor.java
@@ -0,0 +1,34 @@
+package hw_day7_suggestion.data;
+
+public class Mentor {
+	int id;
+	String name;
+	String email;
+	String phone;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	
+	
+}
